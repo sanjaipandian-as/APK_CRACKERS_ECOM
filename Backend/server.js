8f379983b89e20cbe7e51811e3d38dca762c2743
Sanjaipandian-as <sanjaipandian.as@gmail.com>
2026-01-15 12:42:11 +0530

    Add production URL to CORS allowed origins

--- Backend/server.js
+++ Backend/server.js
@@ -27,7 +27,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:5175",
-      "http://localhost:5176"
+      "https://ecomcrackers.vercel.app"
     ],
     credentials: true,
   })
